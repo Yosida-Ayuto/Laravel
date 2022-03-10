e766450f81913191a2bb4d323b827f1c128ab611
Yosida-Ayuto <ksw2170029@stu.o-hara.ac.jp>
2022-03-10 10:59:23 +0900

    3月10日 バリデーション(エラー処理)

--- laravelapp/app/Http/Kernel.php
+++ laravelapp/app/Http/Kernel.php
@@ -35,12 +35,15 @@ class Kernel extends HttpKernel
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            // \Illuminate\Routing\Middleware\SubstituteBindings::class, グローバル登録
         ],
 
         'api' => [
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+        'hello' =>[
+            \App\Http\Middleware\HelloMiddleware::class,
         ],
     ];
 
@@ -62,7 +65,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'hello' => \App\Http\Middleware\HelloMiddleware::class,
+        // 'hello' => \App\Http\Middleware\HelloMiddleware::class, ローカル
     ];
 
     /**
